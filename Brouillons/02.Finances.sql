4b738016c6c9753b0d297df60cba21c8956fc3e9
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2022-09-29 13:32:27 +0200

    Update 02.Finances.sql

--- Brouillons/02.Finances.sql
+++ Brouillons/02.Finances.sql
@@ -1,7 +1,46 @@
 
 -- FINANCES
 
-/* Le chiffre d'affaires des commandes des deux derniers mois par pays */
+/* Question 1 : "Le chiffre d'affaires des commandes des deux derniers mois par pays" */
+
+------------ Prèmiere Solution Pour priceEach*quantityOrdered Comme CA -------------------
+
+SELECT country, month(orders.orderDate) as `month`, 
+year(orders.orderDate) as `year`,sum(orderdetails.priceEach*quantityOrdered) as `Chiffre d'Affaire`
+FROM customers
+Join orders Using (customerNumber)
+Join orderdetails Using (orderNumber)
+Where orders.orderDate >= Date(now() - interval 3 month)
+Group By country, `month`, `year`
+Order By `year`, `month`, country;
+
+-------------------------------------------------------------------------------------------
+------------ Deuxième Solution Pour priceEach*quantityOrdered Comme CA --------------------
+
+WITH mois7 AS (SELECT country, month(orders.orderDate) as `month`, 
+year(orders.orderDate) as `year`,sum(orderdetails.priceEach*quantityOrdered) as `Chiffre d'Affaire`
+FROM customers
+Join orders Using (customerNumber)
+Join orderdetails Using (orderNumber)
+Where year(orders.orderDate) = 2022 and month(orders.orderDate) = 7
+Group By country, `month`, `year`
+Order By `year`, `month`, country),
+
+mois8 AS (SELECT country, month(orders.orderDate) as `month`, 
+year(orders.orderDate) as `year`,sum(orderdetails.priceEach*quantityOrdered) as `Chiffre d'Affaire`
+FROM customers
+Join orders Using (customerNumber)
+Join orderdetails Using (orderNumber)
+Where year(orders.orderDate) = 2022 and month(orders.orderDate) = 8
+Group By country, `month`, `year`
+Order By `year`, `month`, country)
+
+SELECT * FROM mois7
+UNION
+SELECT * FROM mois8;
+
+-------------------------------------------------------------------------------------------------
+----------------------------- Solution Pour amount Comme CA -----------------------------------
 
 SELECT country, amount, paymentDate
 FROM customers
@@ -10,7 +49,12 @@ JOIN orders ON orders.customerNumber=customers.customerNumber
 Where paymentDate >= Date(now() - interval 2 month)
 Group By country;
 
-/* Commandes qui n'ont pas encore été payées */
+
+
+
+
+/* Question 2 : "Commandes qui n'ont pas encore été payées" */
+          
 
 SELECT orderNumber, amount
 FROM customers
